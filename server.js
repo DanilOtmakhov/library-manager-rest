dce10026d5d98f434d8ad2e98f348f9a141c5c58
DanilOtmakhov <danilotm05@gmail.com>
2025-10-16 11:55:33 +0300

    feat: implement homepage ui

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 const express = require("express");
+const path = require("path");
+
 const app = express();
 
 const PORT = 3000;
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use("/books", require("./routes/books"));
 
 app.get("/", (req, res) => {
-  res.send("Welcome to the Library API");
+  res.render("index", { title: "Library Home" });
 });
 
 app.listen(PORT, () => {
